04181e124d53986a3473da42b887fa2a8996d081
peter staal <peterstaal@MBP-van-peter.home>
2024-04-12 09:05:20 +0200

    add sql data and database

---
+++ people.sql
@@ -0,0 +1,11 @@
+CREATE TABLE people (
+    id integer,
+    people_name text,
+    email text
+);
+
+INSERT INTO people VALUES (1, 'Peter Staal', 'peter@bind.nl');
+INSERT INTO people VALUES (2, 'Wietske de Lange', 'wietskedelange@gmail.com');
+INSERT INTO people VALUES (3, 'Joppe Staal', 'joppestaal@gmail.com');
+INSERT INTO people VALUES (4, 'Noud Staal', 'noudstaal@gmail.com');
+INSERT INTO people VALUES (5, 'Atze Staal', 'atze@gmail.com');
